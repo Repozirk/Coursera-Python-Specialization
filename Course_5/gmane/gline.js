a232c00be1d0c201e1b1fc2ba625cef705965cfe
Repozirk <christopher_scherer@gmx.de>
2018-10-17 22:41:03 +0200

    Add files via upload

---
+++ Course_5/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','ufp.pt','ucdavis.edu','indiana.edu','berkeley.edu','uct.ac.za','mac.com','columbia.edu','gmail.com','unl.edu'],
+['2005',57,10,11,12,12,14,12,13,10,12],
+['2006',14,11,10,7,5,2,3,1,2,0]
+];
